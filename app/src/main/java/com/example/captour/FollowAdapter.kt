beff89e38b5f0183574761d78fdf38f6968f4171
7beunseo <kimes0403@gmail.com>
2024-06-14 17:07:45 +0900

    팔로잉 리스트에서 팔로우 취소 - 화면 갱신 필요

--- app/src/main/java/com/example/captour/FollowAdapter.kt
+++ app/src/main/java/com/example/captour/FollowAdapter.kt
@@ -5,8 +5,15 @@ import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.captour.databinding.FollowItemBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MyFollowViewHolder(val binding: FollowItemBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -32,6 +39,34 @@ class FollowAdapter(val datas: List<Follow>?): RecyclerView.Adapter<RecyclerView
                 data = Uri.parse("mailto:${followdata.following}")
             }
             context.startActivity(intent)
+        }
+
+        binding.followCancleBtn.setOnClickListener {
+            // 팔로우 취소
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://172.30.1.4:8080/captour/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val apiService = retrofit.create(NetworkService::class.java)
+            val call =
+                apiService.deleteFollow(
+                    follower = MyApplication.email.toString(),
+                    following = binding.following.text.toString()
+                )
+
+            val context = holder.itemView.context
+
+            call?.enqueue(object : Callback<FollowJsonResponse> {
+                override fun onResponse(call: Call<FollowJsonResponse>, response: Response<FollowJsonResponse>) {
+                    response.body()?.toString()?.let { Log.d("mobileapp", it) }
+                    Toast.makeText(context.applicationContext, "팔로우 삭제 완료", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onFailure(call: Call<FollowJsonResponse>, t: Throwable) {
+                    Toast.makeText(context.applicationContext, "팔로우 삭제 실패", Toast.LENGTH_LONG).show()
+                }
+            })
         }
     }
 }
